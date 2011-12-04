12fb11d27ecd6ae472699a3559806b16fd2020ca
czy88840616 <czy88840616@gmail.com>
2011-12-04 23:35:17 +0800

    test

--- nb/router.js
+++ nb/router.js
@@ -3,8 +3,17 @@
  * @author уем╕ <zhangting@taobao.com>
  *
  */
-function route(pathname) {
-  console.log("About to route a request for " + pathname);
+function route(handle, pathname, response, postData) {
+    console.log("About to route a request for " + pathname);
+    if(typeof handle[pathname] === 'function') {
+        handle[pathname](response, postData);
+        response.end();
+    } else {
+        console.log("No request handler found for " + pathname);
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("404 Not found");
+        response.end();
+    }
 }
 
 exports.route = route;
--- nb/server.js
+++ nb/server.js
@@ -6,16 +6,24 @@
 var http = require("http");
 var url = require('url');
 
-exports.start = function(route) {
+exports.start = function(route, handle) {
     http.createServer(
         function(request, response) {
-             var pathname = url.parse(request.url).pathname;
+            var postData = "";
+            var pathname = url.parse(request.url).pathname;
             console.log("Request for " + pathname + " received.");
-            route(pathname);
-            response.writeHead(200, {"Content-Type": "text/plain"});
-            response.write("good work\n");
-            response.write("good work\n");
-            response.end();
+
+            request.setEncoding("utf8");
+            request.addListener("data", function(postDataChunk) {
+                postData += postDataChunk;
+                console.log("Received POST data chunk '"+
+                    postDataChunk + "'.");
+            });
+
+            request.addListener("end", function() {
+                route(handle, pathname, response, postData);
+            });
+
         }).listen(8888);
     console.log("Server has started.");
 };

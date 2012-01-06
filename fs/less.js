7de62da8d7e5058dc631f38524f1208cc7964538
zhangting <zhangting@taobao.com>
2012-01-06 15:24:56 +0800

    add less

--- fs/less.js
+++ fs/less.js
@@ -1,15 +1,14 @@
 /*
-* 当你保存的时候，lesstocss将你的less文件自动编译成css文件
-* 用法： node less.js
-* version: v0.1 by 飞长(veryued.org)
-* modify by zhangting 修改为监听所有less文件
-**/
+ * 当你保存的时候，lesstocss将你的less文件自动编译成css文件
+ * 用法： node less.js
+ * version: v0.1 by 飞长(veryued.org)
+ * modify by zhangting 修改为监听所有less文件
+ **/
 
 var fs = require('fs'),
     walk = require('walk'),
     exec = require('child_process').exec,
-    path = require('path'),
-    iconv = require('iconv-lite');
+    path = require('path');
 
 //设置lessc路径
 var lessc = path.resolve('./node_modules/.bin/lessc.cmd');
@@ -17,18 +16,18 @@ var lessc = path.resolve('./node_modules/.bin/lessc.cmd');
 var toCss = function(filename) {
     var baseName = path.resolve(path.dirname(filename), path.basename(filename, '.less'));
 
-    exec('' + lessc + ' ' + filename + ' > ' + baseName + '.css', function (err, stdout, stderr) {
-        if (err != null) {
-            err = iconv.decode(err, 'gbk');
-            fs.writeFile(baseName + '.log', err, '', function(error) {
-                if(error) {
-                    console.log('write file error:' + error);
-                }
-            });
-        } else {
-            console.log(baseName + '.css has render.');
-        }
-    });
+    exec('' + lessc + ' ' + filename + ' > ' + baseName + '.css', { encoding: ''},
+        function (err, stdout, stderr) {
+            if (err != null) {
+                fs.writeFile(baseName + '.log', err, '', function(error) {
+                    if(error) {
+                        console.log('write file error:' + error);
+                    }
+                });
+            } else {
+                console.log(baseName + '.css has render.');
+            }
+        });
 };
 
 // Walker options

8f2d6db2c5172aeb028a214661673675e852d738
zhangting <zhangting@taobao.com>
2012-01-06 12:34:57 +0800

    test less full path support

--- fs/watchTest.js
+++ fs/watchTest.js
@@ -3,3 +3,14 @@
  * @author уем╕ <zhangting@taobao.com>
  *
  */
+var fs = require('fs');
+var path = require('path');
+
+console.log(path.resolve('./fs/test.less'));
+
+//test directory watch
+fs.watch('./fs/test.less', function(event, fileName) {
+    if(event === 'change') {
+        console.log('event=%s, fileName=%s', event, fileName);
+    }
+});
